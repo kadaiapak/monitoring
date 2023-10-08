e42893d1f898d1c253202ad6d4c7bdeaabf32030
Denisuardi <denisuardi05@gmail.com>
2023-10-08 13:25:04 +0700

    first commit

---
+++ app/Views/layout/v_footer.php
@@ -0,0 +1,24 @@
+ <!-- footer content -->
+ <footer>
+          <div class="pull-right">
+            Gentelella - Bootstrap Admin Template by <a href="https://colorlib.com">Colorlib</a>
+          </div>
+          <div class="clearfix"></div>
+        </footer>
+        <!-- /footer content -->
+      </div>
+    </div>
+
+    <!-- jQuery -->
+    <script src="<?= base_url(); ?>template/vendors/jquery/dist/jquery.min.js"></script>
+    <!-- Bootstrap -->
+   <script src="<?= base_url(); ?>template/vendors/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+    <!-- FastClick -->
+    <script src="<?= base_url(); ?>template/vendors/fastclick/lib/fastclick.js"></script>
+    <!-- NProgress -->
+    <script src="<?= base_url(); ?>template/vendors/nprogress/nprogress.js"></script>
+    
+    <!-- Custom Theme Scripts -->
+    <script src="<?= base_url(); ?>template/build/js/custom.min.js"></script>
+  </body>
+</html>
